cc9f6ac1624b7abf8daed9e00fbd95597b524938
kieranpark <kieran_afc_2k6@hotmail.com>
2015-10-26 14:42:40 +0000

    folder structure changed

---
+++ helloSector.php
@@ -0,0 +1,6 @@
+<?php
+echo "Where would you like to say hello?";
+    <a href "http://cm3028.azurewebsites.net/helloPrinter.php?planet=Earth"Earth</a>
+    <a href "http://cm3028.azurewebsites.net/helloPrinter.php?planet=Mars"Mars</a>
+    <a href "http://cm3028.azurewebsites.net/helloPrinter.php?planet=Uranus"Uranus</a>
+?>
