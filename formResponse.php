0819693f6ee4048c486bcc575c6c237e86d49268
kieranpark <kieran_afc_2k6@hotmail.com>
2015-10-26 15:28:37 +0000

    folder structure changed

--- formResponse.php
+++ formResponse.php
@@ -1,5 +1,12 @@
 <?php
     $forename=$_POST["forename"];
     $surname=$_POST["surname"];
-    echo $forename . $surname;
+    $dob=$_POST["dob"];
+    $gender=$_POST["gender"];
+    $power=$_POST["power"];
+    echo $forename;
+    echo $surname;
+    echo $dob;
+    echo $gender;
+    echo $power;
 ?>
