9c2d7e45b810d709b37c6f4f86a5f5a0d345d407
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-02 13:43:49 +0000

    folder structure changed

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+    $db = new mysqli('eu-cdbr-azure-north-d.cloudapp.net', 'bab6477022e413', 'fc354109', 'cm3028');
+    if($db->connect_errno){
+        die('Connectfailed['.$db->connect_error.']');
+    }
+    // create a SQL query as a string
+
+    $sql_query = "SELECT * FROM superheros WHERE superpower LIKE '%laser%'";
+
+    // execute the SQL query
+
+    $result = $db->query($sql_query);
+
+    // iterate over $result object one $row at a time
+    // use fetch_array() to return an associative array
+
+    while($row = $result->fetch_array()){
+       // print out fields from row of data
+       echo '<p>' . $row['superheroName'] . '</p>';
+    }
+
+    $result->close();
+       // close connection to database
+       $db->close();
+?>
+
