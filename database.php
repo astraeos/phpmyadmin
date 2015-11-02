262c07b4e0051a3755d53dd23c905c484033559d
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-02 13:54:23 +0000

    folder structure changed

--- database.php
+++ database.php
@@ -1,8 +1,20 @@
 <?php
-    $db = new mysqli('eu-cdbr-azure-north-d.cloudapp.net', 'bab6477022e413', 'fc354109', 'CM3028');
+
+// connect to your server and select database
+
+    $db = new mysqli(
+    'eu-cdbr-azure-north-d.cloudapp.net',
+    'bab6477022e413',
+    'fc354109',
+    'CM3028'
+    );
+
+    // test if connection was established, and print any errors
+
     if($db->connect_errno){
         die('Connectfailed['.$db->connect_error.']');
     }
+
     // create a SQL query as a string
 
     $sql_query = "SELECT * FROM superheros WHERE superpower LIKE '%laser%'";
@@ -16,11 +28,10 @@
 
     while($row = $result->fetch_array()){
        // print out fields from row of data
-       echo '<p>' . $row['superheroName'] . '</p>';
+       echo "<p>" . $row['superheroName'] . "</p>";
     }
 
     $result->close();
        // close connection to database
        $db->close();
 ?>
-
