bed8246ca1b915d5084a41e676d09fb1f6bfaad1
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-09 13:49:04 +0000

    folder structure changed

--- database.php
+++ database.php
@@ -1,37 +1,37 @@
 <?php
 
-// connect to your server and select database
+    // connect to your server and select database
 
     $db = new mysqli(
         "eu-cdbr-azure-north-d.cloudapp.net",
         "bab6477022e413",
         "fc354109",
-        "CM3028"
-    );
+        "marvelmovies"
+);
 
-    // test if connection was established, and print any errors
+// test if connection was established, and print any errors
 
-    if($db->connect_errno){
-        die('Connectfailed['.$db->connect_error.']');
-    }
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect_error.']');
+}
 
-    // create a SQL query as a string
+// create a SQL query as a string
 
-    $sql_query = "SELECT * FROM superheros  WHERE superpower LIKE '%laser%'";
+$sql_query = "SELECT * FROM superheros  WHERE superpower LIKE '%laser%'";
 
-    // execute the SQL query
+// execute the SQL query
 
-    $result = $db->query($sql_query);
+$result = $db->query($sql_query);
 
-    // iterate over $result object one $row at a time
-    // use fetch_array() to return an associative array
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
 
-    while($row = $result->fetch_array()){
-       // print out fields from row of data
-       echo "<p>" . $row['superheroName'] . "</p>";
-    }
+while($row = $result->fetch_array()){
+   // print out fields from row of data
+   echo "<p>" . $row['superheroName'] . "</p>";
+}
 
-    $result->close();
-       // close connection to database
-       $db->close();
-?>
+$result->close();
+   // close connection to database
+   $db->close();
+
